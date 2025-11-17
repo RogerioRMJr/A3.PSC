b2440ec002c521c86146debc78c8461f547c072b
gabriel.franck <gabriel.franck1012@gmail.com>
2025-11-17 20:44:02 -0300

    Todos os arquivos
    Adicionei o protótipo completo do professor. Com DAO, Model, Principal e View

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Principal;
 
-/**
- *
- * @author junior
- */
+import View.TelaPrincipal;
+
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TelaPrincipal objetotela = new TelaPrincipal();
+        objetotela.setVisible(true);
     }
     
 }

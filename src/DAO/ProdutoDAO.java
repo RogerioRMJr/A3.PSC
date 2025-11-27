240acc1c344ff8912a130c412c7a9e99f45b3c90
gabriel.franck <gabriel.franck1012@gmail.com>
2025-11-27 18:03:13 -0300

    Atualização do Model e DAO para nova estrutura de produtos
    Atualiza Produto.java
    Atualiza ProdutoDAO.java

--- src/DAO/ProdutoDAO.java
+++ src/DAO/ProdutoDAO.java
@@ -11,87 +11,74 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.io.IOException;
 
-
 public class ProdutoDAO {
-    
-    public ProdutoDAO() {
-    }
-    
-    // CONEXÃO COM O BANCO 
-    
+
+    public ProdutoDAO() {}
+
+    // ==========================
+    //  CONEXÃO COM BANCO
+    // ==========================
     public Connection getConexao() {
 
-    Connection connection = null;
+        Connection connection = null;
 
-    try {
-        // carrega as configs externas
-        Properties props = loadConfigs();
+        try {
+            Properties props = loadConfigs();
 
-        String host = props.getProperty("db.host");
-        String port = props.getProperty("db.port");
-        String database = props.getProperty("db.name");
-        String user = props.getProperty("db.user");
-        String password = props.getProperty("db.password");
+            String host = props.getProperty("db.host");
+            String port = props.getProperty("db.port");
+            String database = props.getProperty("db.name");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
 
-        String driver = "com.mysql.cj.jdbc.Driver";
-        Class.forName(driver);
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
 
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + database
-                   + "?useTimezone=true&serverTimezone=UTC";
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?useTimezone=true&serverTimezone=UTC";
 
-        connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password);
 
-        if (connection != null) {
-            System.out.println("Status: Conectado!");
-        } else {
-            System.out.println("Status: NÃO CONECTADO!");
+            System.out.println(connection != null ? "Status: Conectado!" : "Status: NÃO CONECTADO!");
+            return connection;
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            return null;
         }
-
-        return connection;
-
-    } catch (ClassNotFoundException e) {
-        System.out.println("Driver não encontrado: " + e.getMessage());
-        return null;
-
-    } catch (SQLException e) {
-        System.out.println("Erro ao conectar: " + e.getMessage());
-        return null;
     }
-}
 
-    
-    //MÉTODO PARA CARREGAR OS DADOS DO BANCO DE FORMA SEGURA
-    
+    // ==========================
+    // CARREGAR CONFIGS
+    // ==========================
     private Properties loadConfigs() {
-    Properties props = new Properties();
-    String fileName = "config.properties";
+        Properties props = new Properties();
+        String fileName = "config.properties";
 
-    try (var input = getClass().getClassLoader().getResourceAsStream(fileName)) {
-        if (input == null) {
-            System.out.println("Arquivo " + fileName + " não encontrado no classpath.");
-            return props;
+        try (var input = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            if (input == null)
+                return props;
+
+            props.load(input);
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar " + fileName + ": " + e.getMessage());
         }
-        props.load(input);
-    } catch (IOException e) {
-        System.out.println("Erro ao carregar " + fileName + ": " + e.getMessage());
+
+        return props;
     }
 
-    return props;
-}
-
-
-    
-    // MAIOR ID 
-    
-    public int maiorID() throws SQLException {
+    // ==========================
+    // MAIOR ID
+    // ==========================
+    public int maiorID() {
         int maiorID = 0;
-        
+
         try {
             Statement stmt = this.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT MAX(id_produto) id FROM tb_produtos");
-            if (res.next()) {
-                maiorID = res.getInt("id");
-            }
+
+            if (res.next()) maiorID = res.getInt("id");
+
             stmt.close();
         } catch (SQLException ex) {
             System.out.println("Erro ao buscar maior ID: " + ex.getMessage());
@@ -99,9 +86,10 @@ public class ProdutoDAO {
 
         return maiorID;
     }
-    
-    // LISTAR TODOS 
-    
+
+    // ==========================
+    // LISTAR TODOS
+    // ==========================
     public ArrayList<Produto> getListaProdutos() {
         ArrayList<Produto> lista = new ArrayList<>();
 
@@ -111,17 +99,19 @@ public class ProdutoDAO {
 
             while (res.next()) {
                 Produto p = new Produto();
+
                 p.setIdProduto(res.getInt("id_produto"));
                 p.setNomeProduto(res.getString("nome_produto"));
                 p.setDescricaoProduto(res.getString("descricao_produto"));
+                p.setCategoria(res.getString("categoria"));
+                p.setFornecedor(res.getString("fornecedor"));
                 p.setQuantidadeEstoque(res.getInt("quantidade_estoque"));
-                p.setPreco(res.getDouble("preco"));
-                
-                // Se estiver usando DATE no banco:
-                if (res.getDate("data_cadastro") != null) {
+                p.setPrecoCusto(res.getDouble("preco_custo"));
+                p.setPrecoVenda(res.getDouble("preco_venda"));
+
+                if (res.getDate("data_cadastro") != null)
                     p.setDataCadastro(res.getDate("data_cadastro").toLocalDate());
-                }
-                
+
                 lista.add(p);
             }
 
@@ -133,12 +123,17 @@ public class ProdutoDAO {
 
         return lista;
     }
-    
-    //  INSERIR 
-    
-    public boolean inserirProdutoBD(Produto p) throws SQLException {
-        String sql = "INSERT INTO tb_produtos(id_produto, nome_produto, descricao_produto, quantidade_estoque, preco, data_cadastro) "
-                   + "VALUES(?,?,?,?,?,?)";
+
+    // ==========================
+    // INSERIR PRODUTO
+    // ==========================
+    public boolean inserirProdutoBD(Produto p) {
+        String sql = """
+        INSERT INTO tb_produtos
+        (id_produto, nome_produto, descricao_produto, categoria, fornecedor,
+        quantidade_estoque, preco_custo, preco_venda, data_cadastro)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """;
 
         try {
             PreparedStatement stmt = this.getConexao().prepareStatement(sql);
@@ -149,15 +144,16 @@ public class ProdutoDAO {
             stmt.setInt(1, p.getIdProduto());
             stmt.setString(2, p.getNomeProduto());
             stmt.setString(3, p.getDescricaoProduto());
-            stmt.setInt(4, p.getQuantidadeEstoque());
-            stmt.setDouble(5, p.getPreco());
-            
-            // dataCadastro já vem setada no construtor com LocalDate.now();
-            if (p.getDataCadastro() != null) {
-                stmt.setDate(6, java.sql.Date.valueOf(p.getDataCadastro()));
-            } else {
-                stmt.setDate(6, null);
-            }
+            stmt.setString(4, p.getCategoria());
+            stmt.setString(5, p.getFornecedor());
+            stmt.setInt(6, p.getQuantidadeEstoque());
+            stmt.setDouble(7, p.getPrecoCusto());
+            stmt.setDouble(8, p.getPrecoVenda());
+
+            if (p.getDataCadastro() != null)
+                stmt.setDate(9, java.sql.Date.valueOf(p.getDataCadastro()));
+            else
+                stmt.setDate(9, null);
 
             stmt.execute();
             stmt.close();
@@ -168,9 +164,10 @@ public class ProdutoDAO {
             throw new RuntimeException(erro);
         }
     }
-    
-    //  DELETAR 
-    
+
+    // ==========================
+    // DELETAR
+    // ==========================
     public boolean deleteProdutoBD(int idProduto) {
         try {
             Statement stmt = this.getConexao().createStatement();
@@ -179,28 +176,32 @@ public class ProdutoDAO {
         } catch (SQLException erro) {
             System.out.println("Erro ao deletar produto: " + erro.getMessage());
         }
-        
+
         return true;
     }
-    
-    //  ATUALIZAR 
-    
+
+    // ==========================
+    // ATUALIZAR
+    // ==========================
     public boolean updateProdutoBD(Produto p) {
-        String sql = "UPDATE tb_produtos SET "
-                   + "nome_produto = ?, "
-                   + "descricao_produto = ?, "
-                   + "quantidade_estoque = ?, "
-                   + "preco = ? "
-                   + "WHERE id_produto = ?";
+        String sql = """
+        UPDATE tb_produtos SET
+        nome_produto = ?, descricao_produto = ?, categoria = ?, fornecedor = ?,
+        quantidade_estoque = ?, preco_custo = ?, preco_venda = ?
+        WHERE id_produto = ?
+        """;
 
         try {
             PreparedStatement stmt = this.getConexao().prepareStatement(sql);
 
             stmt.setString(1, p.getNomeProduto());
             stmt.setString(2, p.getDescricaoProduto());
-            stmt.setInt(3, p.getQuantidadeEstoque());
-            stmt.setDouble(4, p.getPreco());
-            stmt.setInt(5, p.getIdProduto());
+            stmt.setString(3, p.getCategoria());
+            stmt.setString(4, p.getFornecedor());
+            stmt.setInt(5, p.getQuantidadeEstoque());
+            stmt.setDouble(6, p.getPrecoCusto());
+            stmt.setDouble(7, p.getPrecoVenda());
+            stmt.setInt(8, p.getIdProduto());
 
             stmt.execute();
             stmt.close();
@@ -211,34 +212,37 @@ public class ProdutoDAO {
             throw new RuntimeException(erro);
         }
     }
-    
-    //  CARREGAR UM ÚNICO PRODUTO 
-    
+
+    // ==========================
+    // CARREGAR UM PRODUTO
+    // ==========================
     public Produto carregaProduto(int idProduto) {
         Produto p = new Produto();
         p.setIdProduto(idProduto);
-        
+
         try {
             Statement stmt = this.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_produtos WHERE id_produto = " + idProduto);
-            
+
             if (res.next()) {
                 p.setNomeProduto(res.getString("nome_produto"));
                 p.setDescricaoProduto(res.getString("descricao_produto"));
+                p.setCategoria(res.getString("categoria"));
+                p.setFornecedor(res.getString("fornecedor"));
                 p.setQuantidadeEstoque(res.getInt("quantidade_estoque"));
-                p.setPreco(res.getDouble("preco"));
-                
-                if (res.getDate("data_cadastro") != null) {
+                p.setPrecoCusto(res.getDouble("preco_custo"));
+                p.setPrecoVenda(res.getDouble("preco_venda"));
+
+                if (res.getDate("data_cadastro") != null)
                     p.setDataCadastro(res.getDate("data_cadastro").toLocalDate());
-                }
             }
 
-            stmt.close();            
-            
+            stmt.close();
+
         } catch (SQLException erro) {
             System.out.println("Erro ao carregar produto: " + erro.getMessage());
         }
-        
+
         return p;
     }
 }
--- src/Model/Produto.java
+++ src/Model/Produto.java
@@ -3,26 +3,39 @@ package Model;
 import java.time.LocalDate;
 
 public class Produto {
-    
+
     private int idProduto;
     private String nomeProduto;
     private String descricaoProduto;
+    private String categoria;
+    private String fornecedor;
     private int quantidadeEstoque;
-    private double preco;
+    private double precoCusto;
+    private double precoVenda;
     private LocalDate dataCadastro;
-    
-    public Produto (){
+
+    // Construtor vazio (obrigatório para DAO)
+    public Produto() {
     }
 
-    public Produto(int idProduto, String nomeProduto, String descricaoProduto, int quantidadeEstoque, double preco) {
+    // Construtor completo (para criar objetos mais facilmente)
+    public Produto(int idProduto, String nomeProduto, String descricaoProduto, String categoria,
+                   String fornecedor, int quantidadeEstoque, double precoCusto, double precoVenda) {
+
         this.idProduto = idProduto;
         this.nomeProduto = nomeProduto;
         this.descricaoProduto = descricaoProduto;
+        this.categoria = categoria;
+        this.fornecedor = fornecedor;
         this.quantidadeEstoque = quantidadeEstoque;
-        this.preco = preco;
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+
+        // já define a data de cadastro automaticamente
         this.dataCadastro = LocalDate.now();
     }
 
+    // GETTERS e SETTERS
     public int getIdProduto() {
         return idProduto;
     }
@@ -47,6 +60,22 @@ public class Produto {
         this.descricaoProduto = descricaoProduto;
     }
 
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
     public int getQuantidadeEstoque() {
         return quantidadeEstoque;
     }
@@ -55,12 +84,20 @@ public class Produto {
         this.quantidadeEstoque = quantidadeEstoque;
     }
 
-    public double getPreco() {
-        return preco;
+    public double getPrecoCusto() {
+        return precoCusto;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
     }
 
     public LocalDate getDataCadastro() {
@@ -71,18 +108,27 @@ public class Produto {
         this.dataCadastro = dataCadastro;
     }
 
-    public double getValorTotal(){
-        return preco * quantidadeEstoque;
+    // LUCRO do produto (útil na tabela)
+    public double getLucro() {
+        return precoVenda - precoCusto;
     }
-    
+
+    // Total em estoque (se quiser usar)
+    public double getValorTotal() {
+        return precoVenda * quantidadeEstoque;
+    }
+
     @Override
-    public String toString(){
-        return "Produto { " + 
-               "id=" + idProduto + 
-               ", nome='" + nomeProduto + '\'' + 
-               ", quantidade=" + quantidadeEstoque + 
-               ", preco=" + preco + 
-               ", dataCadastro=" + dataCadastro + 
-               " }";
+    public String toString() {
+        return "Produto { " +
+                "id=" + idProduto +
+                ", nome='" + nomeProduto + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", fornecedor='" + fornecedor + '\'' +
+                ", estoque=" + quantidadeEstoque +
+                ", custo=" + precoCusto +
+                ", venda=" + precoVenda +
+                ", cadastro=" + dataCadastro +
+                " }";
     }
 }
